ebdf728085232b69fd3adee9f935729aee7be692
Vadim Vetrov <vetrovvd@gmail.com>
2025-09-15 23:34:37 +0300

    Mergesort with only one allocation

--- src/sort.cpp
+++ src/sort.cpp
@@ -59,7 +59,6 @@ static int mergePvector(
 		}
 	}
 
-	assert(merge_storage.len == right - left);
 	for (size_t arr_idx = left, merge_idx = 0; 
 		arr_idx < right; arr_idx++, merge_idx++) {
 		arr->arr[arr_idx] = merge_storage.arr[merge_idx];	
@@ -96,18 +95,84 @@ static int mergeSortRecursive(
 	return 0;
 }
 
+/**
+ * Like mergePvector but with preallocated memory.
+ * mergedVectors should be at least ((right - left) * sizeof(void *)) bytes long
+ */
+static int mergePvector_with_memory(
+	struct pvector *lines_arr, 
+	size_t left, size_t mid, size_t right, 
+	pv_sort_comparator comparator,
+	void **mergedVectors
+) {
+	assert (lines_arr);
+	assert (comparator);
+	assert (mergedVectors);
+
+	size_t left_idx = left, right_idx = mid;
+	size_t merged_idx = 0;
+	while (left_idx < mid || right_idx < right) {
+		if (left_idx >= mid) {
+			mergedVectors[merged_idx++] = lines_arr->arr[right_idx++];
+		} else if (right_idx >= right) {
+			mergedVectors[merged_idx++] = lines_arr->arr[left_idx++];
+		} else {
+			int cmp = comparator(lines_arr->arr[left_idx], lines_arr->arr[right_idx]);
+
+			if (cmp) {
+				mergedVectors[merged_idx++] = lines_arr->arr[right_idx++];
+			} else {
+				mergedVectors[merged_idx++] = lines_arr->arr[left_idx++];
+			}
+		}
+	}
+
+	for (size_t arr_idx = left, m_idx = 0; 
+		arr_idx < right; arr_idx++, m_idx++) {
+		lines_arr->arr[arr_idx] = mergedVectors[m_idx];
+	}
+
+	return 0;
+}
 
 int pvector_merge_sort(pvector *lines_arr, pv_sort_comparator comparator) {
 	assert (lines_arr);
 	assert (comparator);
 
-	if (lines_arr->len == 0) {
+	size_t arr_len = lines_arr->len;
+
+	if (arr_len == 0) {
 		return 0;
 	}
 
-	if (mergeSortRecursive(lines_arr, comparator, 
-			0, lines_arr->len)) {
+	// A recursive realisation for reference, use iterative
+	// if (mergeSortRecursive(lines_arr, comparator, 
+	// 		0, lines_arr->len)) {
+	// 	return -1;
+	// }
+
+	void **mergedVectors = (void **)calloc(arr_len, sizeof(void *));
+	if (!mergedVectors) {
+		perror("calloc mergedVectors");
 		return -1;
 	}
+
+
+	for (size_t i = 1; i < arr_len; i *= 2) {
+		for (size_t j = 0; j < arr_len - i; j += 2 * i) {
+			size_t left	= j;
+			size_t mid	= j + i;
+			size_t right	= j + i + i;
+
+			if (right > arr_len)
+				right = arr_len;
+
+			mergePvector_with_memory(lines_arr, left, mid, right, 
+				comparator, mergedVectors);
+		}
+	}
+
+	free(mergedVectors);
+	
 	return 0;
 }
